b767938c1649e352e280df60e56f99abbf07dc9f
Maria Demchenko <maria.demchenko@gmail.com>
2016-02-27 17:35:01 +0200

    new file

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def about(request):
     return render(request, 'blog/about_blog.html', {})
+
+def post_detail(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
